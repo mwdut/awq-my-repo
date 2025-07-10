f376aaa593e1572d2faf6317e4125a3d7364443f
mwdut <mwdutrav@gmail.com>
2025-07-10 16:38:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-import os
+import os, getpass
+from huggingface_hub import login, create_repo, snapshot_download, upload_file, HfApi
 
-from huggingface_hub import snapshot_download
+token = getpass.getpass("HF Write Token: ")
+login(token=token)
 
-hf_model = input("Insert Model Hub ID ")
+src_repo = input("HF Model ID: ")
 
-snapshot_download(repo_id=hf_model, local_dir='./model-d')
+api = HfApi()
+user = api.whoami()['name']
+dest_repo = f"{user}/{src_repo.split('/')[-1]}-GPTQ"
+local_dir = src_repo.replace('/', '_')
 
-print("Model downloaded successully!")
+create_repo(repo_id=dest_repo, exist_ok=True)
+
+snapshot_download(repo_id=src_repo,
+    local_dir=local_dir,
+    allow_patterns=["*.md", "*model.safetensors", "tokenizer.json , config.json , generation_config.json"]
+)
+
+for f in os.listdir(local_dir):
+    path_model = os.path.join(local_dir, f)
+    if os.path.isfile(path_model):
+        upload_file(
+            path_or_fileobj=path_model,
+            path_in_repo=f,
+            repo_id=dest_repo
+        )
+
+print(f"Sucessfull!!!: Upolad to  https://huggingface.co/{dest_repo}")
