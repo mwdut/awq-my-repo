9401047907cd139c64c7b5f2081618a8e5ab6c1b
mwdut <mwdutrav@gmail.com>
2025-07-11 16:03:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,23 @@ from huggingface_hub import login, create_repo, snapshot_download, upload_folder
 
 token = getpass.getpass("HF Write Token: ")
 login(token=token)
+
 hf_model = input("HF Model ID: ")
 
 api = HfApi()
 user = api.whoami()['name']
-
-
-repo_name = f"{user}/{hf_model.split('/')[-1]}-GPTQ"
+dest_repo = f"{user}/{hf_model.split('/')[-1]}-GPTQ"
 local_dir = hf_model.replace('/', '_')
+
+create_repo(repo_id=dest_repo, exist_ok=True)
 
 snapshot_download(repo_id=hf_model, local_dir=local_dir, allow_patterns=["*.md", "*model.safetensors", "tokenizer.json , config.json , generation_config.json"])
 
-
-create_repo(repo_id=repo_name, exist_ok=True)
 upload_folder(
     folder_path=local_dir,
-    repo_id=repo_name
+    repo_id=dest_repo
 )
 
 shutil.rmtree(local_dir, ignore_errors=True)
 
-print(f": Upolad to  https://huggingface.co/{repo_name}")
+print(f": Upolad to  https://huggingface.co/{dest_repo}")
