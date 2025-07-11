5b54767d1a90721a65db8b93b491b7e8140f7342
mwdut <mwdutrav@gmail.com>
2025-07-10 23:03:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os, getpass
-from huggingface_hub import login, create_repo, snapshot_download, upload_file, HfApi
+from huggingface_hub import login, create_repo, snapshot_download, upload_folder, HfApi
 
 token = getpass.getpass("HF Write Token: ")
 login(token=token)
@@ -15,13 +15,11 @@ create_repo(repo_id=dest_repo, exist_ok=True)
 
 snapshot_download(repo_id=src_repo, local_dir=local_dir, allow_patterns=["*.md", "*model.safetensors", "tokenizer.json , config.json , generation_config.json"])
 
-for f in os.listdir(local_dir):
-    path_model = os.path.join(local_dir, f)
-    if os.path.isfile(path_model):
-        upload_file(
-            path_or_fileobj=path_model,
-            path_in_repo=f,
-            repo_id=dest_repo
-        )
+upload_folder(
+    folder_path=local_dir,
+    repo_id=dest_repo
+)
 
-print(f"Sucessfull!!!: Upolad to  https://huggingface.co/{dest_repo}")
+shutil.rmtree(local_dir, ignore_errors=True)
+
+print(f": Upolad to  https://huggingface.co/{dest_repo}")
